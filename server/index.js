1473fd4ec9447b81f3ec1377ee2b9a74f3d45d07
Aniketh Reddy <sunnyrockzzz1811@gmail.com>
2025-10-28 13:25:51 +0530

    Add server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import robot from "robotjs"
+import express from "express"
+
+const app = express()
+const PORT = 3000 
+
+app.get("/", (req, res)=> res.send("Hello World!"))
+
+app.listen(PORT, console.log("Server running on http://localhost:"+PORT))
